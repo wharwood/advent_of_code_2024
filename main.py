a4ca3a6ae6783bbe98e42d96de02f39f99aa2f3e
William Harwood <harwood.william@gmail.com>
2025-07-12 11:59:26 -0700

    Added main

---
+++ main.py
@@ -0,0 +1,10 @@
+from day01 import day01
+from day02 import day02
+from helpers import helpers
+
+# input_lines = helpers.parse_input("day01/example1.txt")
+# day01.do_part_1(input_lines)
+# day01.do_part_2(input_lines)
+input_lines = helpers.parse_input("day02/example1.txt")
+# day02.do_part_1(input_lines)
+day02.do_part_2(input_lines)
